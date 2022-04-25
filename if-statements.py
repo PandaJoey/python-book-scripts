3c2bf0783331af535bf46fea2f473c8a77fdd484
PandaJoey <pandajoey77@gmail.com>
2022-04-25 12:24:23 +0100

    added new code to rep

---
+++ if-statements.py
@@ -0,0 +1,186 @@
+"""
+#different kinds of checks
+
+cars = ['audi', 'bmw', 'subaru', 'toyota']
+
+for car in cars:
+    if car == 'bmw':
+        print(car.upper())
+    else:
+        print(car.title())
+
+requested_topping = 'mushrooms'
+if requested_topping != 'anchovies':
+    print("Hold the anchovies!")
+
+#numerical comparisions 
+
+answer = 17
+if answer != 42:
+    print("That is not the correct answer. Please try again!")
+
+age = 19
+print(age < 21)
+print(age <= 21)
+print(age > 21)
+print(age >= 21)
+
+#multiple conditions
+
+age_0 = 22
+age_1 = 18
+print(age_0 >= 21 and age_1 >= 21)
+
+age_1 = 22
+print(age_0 >= 21 and age_1 >= 21)
+
+age_0 = 22
+age_1 = 18
+print(age_0 >= 21 or age_1 >= 21)
+
+age_0 = 18
+print(age_0 >= 21 or age_1 >= 21)
+
+#list comparisions
+requested_toppings = ['mushrooms', 'onions', 'pineapple']
+'mushrooms' in requested_toppings
+'pepperoni' in requested_toppings
+
+banned_users = ['andrew', 'carolina', 'david']
+user = 'marie'
+
+if user not in banned_users:
+    print(f"{user.title()}, you can post a response if you wish.")
+
+#booleans
+
+game_active = True
+can_edit = False
+
+#ex 5.1
+car = 'subaru'
+print("Is car == 'subaru'? I predict True.")
+print(car == 'subaru')
+print("\nIs car == 'audi'? I predict False.")
+print(car == 'audi')
+
+num_1 = 15
+num_2 = 25
+bool_1 = True
+bool_2 = False
+print(num_1 != num_2)
+print(num_1 < num_2)
+print(num_1 <= num_2)
+print(num_1 <= 15 and num_2 >= num_1)
+print(bool_1 != bool_2)
+
+print(num_1 == num_2)
+print(num_1 > num_2)
+print(num_1 >= num_2)
+print(num_1 >= 15 and num_2 <= num_1)
+print(bool_1 == bool_2)
+
+string_1 = "Hello"
+string_2 = "world"
+string_3 = "hello"
+print(string_1 == string_2)
+print(string_1.lower() == string_3)
+
+#if statements
+age = 19
+if age >= 18:
+    print("You are old enough to vote!")
+    print("Have you registered to vote yet?")
+
+#if-else statements
+age = 17
+if age >= 18:
+    print("You are old enough to vote!")
+    print("Have you registered to vote yet?")
+else:
+    print("Sorry, you are too young to vote.")
+    print("Please register to vote as soon as you turn 18!")
+
+#if-elif-else
+age = 12
+if age < 4:
+    price = 0
+elif age < 18:
+    price = 25
+elif age < 65:
+    price = 40
+elif age >= 65:
+    price = 25
+else:
+    price = 40
+
+print(f"Your admission cost is ${price}")
+
+
+
+requested_toppings = ['mushrooms', 'extra cheese']
+if 'mushrooms' in requested_toppings:
+    print("Adding mushrooms.")
+if 'extra cheese' in requested_toppings:
+    print("Adding extra cheese.")
+if 'pepperoni' in requested_toppings:
+    print("Adding pepperoni.")
+
+print("\nFinished making your pizza!")
+
+#doesnt work as intended as it breaks out after the first true
+requested_toppings = ['mushrooms', 'extra cheese']
+if 'mushrooms' in requested_toppings:
+    print("Adding mushrooms.")
+elif 'pepperoni' in requested_toppings:
+    print("Adding pepperoni.")
+elif 'extra cheese' in requested_toppings:
+    print("Adding extra cheese.")
+print("\nFinished making your pizza!")
+
+
+#ex 5.3 and 5.4 and 5.5
+alien_colour = "red"
+if alien_colour == "green":
+    print(f"The alien is {alien_colour}, well done you get 5 points")
+elif alien_colour == "yellow":
+    print(f"you just earnth 10 points for killing the {alien_colour} alien")
+else:
+    print(f"then alient was {alien_colour}, you get 15 points!")
+
+if alien_colour == "red":
+    pass
+
+age = 66
+
+if age < 2:
+    print("this person is a baby")
+elif age >=2 and age < 4:
+    print("this person is a toddler")
+elif age >= 4 and age < 13:
+    print("this person kid")
+elif age >= 13 and age < 20:
+    print("this person is a teenager")
+elif age >= 20 and age < 65:
+    print("this person is an adult")
+else:
+    print("this person is an elder")
+
+fav_fruit = ["strawberry", "blueberrys", "raspberry", "blackberry", "gooseberrys"]
+if 'strawberry' in fav_fruit:
+    print("you really like Strawberries")
+if 'blueberrys' in fav_fruit:
+    print("you really like blueberrys")
+if 'raspberry' in fav_fruit:
+    print("you really like raspberry")
+if 'blackberry' in fav_fruit:
+    print("you really like blackberry")
+if 'gooseberrys' in fav_fruit:
+    print("you really like gooseberrys")
+
+"""
+
+requested_toppings = ['mushrooms', 'green peppers', 'extra cheese']
+for requested_topping in requested_toppings:
+    print(f"Adding {requested_topping}.")
+print("\nFinished making your pizza!")
--- loops.py
+++ loops.py
@@ -1,5 +1,5 @@
 import datetime
-"""
+
 magicians = ['alice', 'david', 'carolina']
 for magician in magicians:
     print(f"{magician.title()}, that was a great trick!")
@@ -46,17 +46,10 @@ for i in range(0, 1_000_001):
 print(datetime.datetime.now())
 print(datetime.datetime.now() - begin_time)
 
-4-5. Summing a Million: Make a list of the numbers from one to one million,
-and then use min() and max() to make sure your list actually starts at one and
-ends at one million. Also, use the sum() function to see how quickly Python can
-add a million numbers.
-
-
 million = list(range(1, 1_000_001))
 print(min(million))
 print(max(million))
 print(sum(million))
-
 
 for i in range(1, 21, 2):
     print(i)
@@ -69,7 +62,6 @@ for i in range(1, 11):
 
 cubes = [value**3 for value in range(1, 11)]
 print(cubes)
-"""
 
 #list slicing
 
@@ -77,3 +69,69 @@ players = ['charles', 'martina', 'michael', 'florence', 'eli']
 print(players[0:3])
 print(players[1:4])
 print(players[:4])
+print(players[2:])
+print(players[-3:])
+
+print("Here are the first three players on my team:")
+for player in players[:3]:
+    print(player.title())
+
+my_foods = ['pizza', 'falafel', 'carrot cake', 'chocolate']
+friend_foods = my_foods[:]
+
+my_foods.append('cannoli')
+friend_foods.append('ice cream')
+print("My favorite foods are:")
+print(my_foods)
+print("\nMy friend's favorite foods are:")
+print(friend_foods)
+
+#ex 4.10
+print(f"The first three items in this list are {my_foods[:3]}")
+print(f"The three items in middle of the list are {my_foods[2:5]}")
+print(f"The last 3 lines of the list are {my_foods[-3:]}")
+
+#ex 4.11
+pizzas = ["pepperoni", "goats cheese", "american hot"]
+friend_pizza = pizzas[:]
+pizzas.append("ham and pineapple")
+friend_pizza.append("vegeterian")
+for pizza in pizzas:
+    print(f"I like {pizza} pizza")
+print(f"I really love {pizzas[1]} pizza")
+for pizza in friend_pizza:
+    print(f"I like {pizza} pizza")
+print(f"I really love {friend_pizza[2]} pizza")
+
+#tuples
+
+dimensions = (200, 50)
+print(dimensions[0])
+print(dimensions[1])
+#causes error as you cant assign to tuples
+#dimensions[0] = 250
+#if you only want one item for some reason leave a comma after the first element
+my_t = (3,)
+
+for dimension in dimensions:
+    print(dimension)
+
+print("Original dimensions:")
+for dimension in dimensions:
+    print(dimension)
+
+dimensions = (400, 100)    
+print("\nModified dimensions:")
+for dimension in dimensions:
+    print(dimension)
+
+food = ("wings", "ribs", "steaks", "bacon", "cheese")
+for item in food:
+    print(f"We offer these things from the menu {item}")
+    #food[0] = "salad"
+
+food = ("wings", "vegan", "steaks", "bacon", "salad")
+for item in food:
+    print(f"We offer these things from the menu {item}")
+
+
